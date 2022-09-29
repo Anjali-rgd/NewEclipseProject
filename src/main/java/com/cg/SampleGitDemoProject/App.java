1767648430f3d66c8fa96be154371ccdc0b41cec
anjmane <anjmane@INLEN8520067331.corp.capgemini.com>
2022-09-29 16:26:33 +0530

    This is first eclipse project to be pushed

---
+++ src/main/java/com/cg/SampleGitDemoProject/App.java
@@ -0,0 +1,16 @@
+package com.cg.SampleGitDemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    
+    {
+    	int a=10; 
+    	System.out.println(a);
+        System.out.println( "Hello World!" );
+    }
+}
